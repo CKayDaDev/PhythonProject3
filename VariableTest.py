e4b10f90477f0891c96c4ea0672344339cb61eac
CKayDaDev <chikeechendu@gmail.com>
2025-08-30 17:49:04 +0100

    Add files via upload

---
+++ VariableTest.py
@@ -0,0 +1,7 @@
+myGreatNumber = 10
+print(myGreatNumber)
+MyFavouriteFood = "pancakes."
+print("My favourite food is " + MyFavouriteFood)
+
+UserFavouriteFood = input("What is your favourite food")
+print("/nYour Favourite Food is ", UserFavouriteFood)
